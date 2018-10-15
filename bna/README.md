f2e5af7e3727b0a0c8efb325efc02cf256c55236
huongnhd <huongnhd@lifull-tech.vn>
2018-10-15 09:45:53 +0700

    adding bna module

---
+++ bna/README.md
@@ -0,0 +1,1 @@
+# Bussiness Network Application
---
+++ bna/lib/model.cto.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* global getAssetRegistry getParticipantRegistry getFactory */
+
+/**
+ *
+ * @transaction Citizen open complaint
+ */
+async function onOpenComplaint(complaint) {  // eslint-disable-line no-unused-vars
+    console.log('processing in ope complaint');
+   
+}
+
+/**
+ *
+ * @transaction Deparment change status of complaint
+ */
+async function onChangeStatusComplaint(status) {  // eslint-disable-line no-unused-vars
+    console.log('processing on change status');
+}
