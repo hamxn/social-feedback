ca7024a0e895a0b691f81c70c0d2e50411ae8f6d
Nguyễn Xuân Hàm <hamxn@lifull-tech.vn>
2018-10-30 11:52:12 +0700

    remove debugbar provider

--- frontend/config/app.php
+++ frontend/config/app.php
@@ -160,7 +160,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\AdminServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -209,7 +208,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
