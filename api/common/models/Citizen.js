a441f810c8019792dd744b442557540a7649d667
Nguyễn Xuân Hàm <hamxn@lifull-tech.vn>
2018-12-26 13:13:33 +0700

    Remove folder api

--- api/common/models/Citizen.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const Composer = require('../lib/composer.js');
-
-module.exports = function(Citizen) {
-  Composer.restrictModelMethods(Citizen);
-};
--- api/common/models/OpenComplaint.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const Composer = require('../lib/composer.js');
-
-module.exports = function(OpenComplaint) {
-  Composer.restrictModelMethods(OpenComplaint);
-};
